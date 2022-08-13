0fd288d449ea6457c4df2002cb7428503a875c07
KvRae <karamelmannai@gmail.com>
2022-08-14 00:07:36 +0100

    Updating styling

---
+++ assets/js/Loading-Page-Animation-Style.js
@@ -0,0 +1,4 @@
+window.onload = function () {
+    var de = document.getElementById("LoadingPage");
+    de.style.display = 'none';
+};
--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -3,6 +3,7 @@ if (window.innerWidth < 768) {
 }
 
 $(document).ready(function(){
+	AOS.init();
 
 	$('[data-bss-hover-animate]')
 		.mouseenter( function(){ var elem = $(this); elem.addClass('animated ' + elem.attr('data-bss-hover-animate')) })
