3ba949f886ff647515e4aea65cd8e13e688267aa
KvRae <karamelmannai@gmail.com>
2025-09-18 19:37:34 +0100

    Fixing gh-pages base path"

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+
   plugins: [
       tailwindcss(),
-      react()
+      react(),
+
   ],
+    base: './',
 })
